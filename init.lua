3f142dade1fbeef5f3932d4b096d2ee887a8ee56
Mārtiņš Jakubovičs <martins.jakubovics@mintos.com>
2024-02-01 14:17:28 +0200

    Changes

--- init.lua
+++ init.lua
@@ -209,6 +209,14 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 4,
+          }
+        }
+      }
     },
   },
 
